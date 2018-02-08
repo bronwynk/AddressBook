8ad2ccc712acf8abbdecf75b82b6d9c74599d5bc
Bronwyn Skelley <bronwynskelley@LABS.SCE.CARLETON.CA>
2018-02-08 11:26:43 -0500

    commit

--- src/test/java/addressBook/ApplicationTest.java
+++ src/test/java/addressBook/ApplicationTest.java
@@ -1,16 +1,31 @@
 package addressBook;
 
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class ApplicationTest {
 
-    @Test
-    public void contextLoads() throws Exception {
-    }
+    @Autowired
+    private MockMvc mockMvc;
 
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(post("/book")).andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("id", is(0)));
+    }
 }
